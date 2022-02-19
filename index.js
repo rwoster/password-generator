b792b615e69da154ef258613625b6fe45ee48965
Randy W <rwoster@protonmail.com>
2022-02-18 20:24:09 -0500

    setup, w/o js

---
+++ index.js
@@ -0,0 +1,9 @@
+let password1 = document.getElementById("password1");
+let password2 = document.getElementById("password2");
+let password3 = document.getElementById("password3");
+let password4 = document.getElementById("password4");
+
+password1.textContent = "0123456789012";
+// password2.textContent = "";
+// password3.textContent = "";
+// password4.textContent = "";
